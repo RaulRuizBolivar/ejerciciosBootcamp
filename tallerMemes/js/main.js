df3a6a6887f3d345c3344d764a6dcca60476d26f
Raúl Ruiz <raul.ruiz.bolivar@gmail.com>
2022-06-02 08:56:26 +0200

    He refactorizado dandole la responsabilidad que tiene a cada funcion

--- tallerMemes/js/main.js
+++ tallerMemes/js/main.js
@@ -1,6 +1,6 @@
 const url = 'https://api.giphy.com/v1/gifs/search?lang=es&q='
 const urlTrending = 'https://api.giphy.com/v1/gifs/trending?'
-const token = 'BaSL4UINo562DAc55frWf3OkgCoKHfG7'
+const token = '&api_key=BaSL4UINo562DAc55frWf3OkgCoKHfG7'
 const inputBusqueda = document.querySelector( '#search .search' )
 const sectionMemes = document.querySelector( '#memes' )
 
@@ -8,28 +8,27 @@ inputBusqueda.addEventListener( 'keydown', recogerTecla )
 
 function recogerTecla ( event ) {
     if ( event.keyCode === 13 ) {
-        buscarMemes( url, token, sectionMemes, inputBusqueda.value )
+        buscarMemes( url, token, inputBusqueda.value )
     }
 }
-async function pintarTodosMemes ( url, token, dom, busqueda ) {
+async function pintarTodosMemes ( busqueda ) {
     inputBusqueda.value = ''
     sectionMemes.innerHTML = ''
-    let memes = await buscarMemes( url, token, busqueda )
-    memes.data.forEach( meme => pintarUnMeme( meme, dom ) );
+    console.log( busqueda )
+    busqueda.forEach( meme => pintarUnMeme( meme ) );
 }
-async function buscarTrending () {
-    const result = await fetch( urlTrending, { method: 'get' } )
+async function buscarTrending ( url, token ) {
+    const result = await fetch( url + token, { method: 'get' } )
     const gifs = await result.json()
     pintarTodosMemes( gifs.data )
 }
 async function buscarMemes ( url, token, busqueda ) {
-    urlFinal = url + busqueda + '&api_key=' + token
+    urlFinal = url + busqueda + token
     let response = await fetch( urlFinal, { method: 'get' } )
     let memes = await response.json()
-    return memes
+    pintarTodosMemes( memes.data )
 }
-
-function pintarUnMeme ( meme, dom ) {
+function pintarUnMeme ( meme ) {
     let article = document.createElement( 'article' )
     let h2 = document.createElement( 'h2' )
     h2.innerText = meme.username
@@ -39,9 +38,9 @@ function pintarUnMeme ( meme, dom ) {
     let p = document.createElement( 'p' )
     p.innerText = meme.title
     article.append( h2, img, p )
-    dom.appendChild( article )
+    sectionMemes.appendChild( article )
 }
-
+buscarTrending( urlTrending, token )
 //Quiero que me hagais un buscador de gifs, el buscador funciona cuando demos al intro
 //En la seccion correspondiente me pintaran todos los gifs de la busqueda
 //Bola curva: pintarme inicialmente los gifs trending
